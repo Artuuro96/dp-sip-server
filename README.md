6b97df8bb95c7b8bc51767ebeacf28765444dfd5
Arturo Rodriguez <arturorodr96@gmail.com>
2023-01-17 00:36:37 -0600

    Initializing with lerna promer project

---
+++ README.md
@@ -0,0 +1,32 @@
+# dp-sip-server
+
+### Prerequisites
+
+The things you have to do in order to install the software and how to install them.
+
+* Node 16+
+* Npm 8+
+
+### Installing
+Clone this project on your computer
+
+```
+https://github.com/grupo-promer/dp-sip-server.git
+```
+then 
+```
+$ cd ./dp-sip-server/
+$ npm install
+$ npm run bootstrap:dev
+```
+
+The `npm run bootstrap:dev` is going to clean the existing dependencies (node_modules) and then bootstrap and build all the lerna packages.
+
+## Code Style
+Code style help us to keep the same format and style when we code, we use Linter which is a tool that is able to detec bugs, style errors, etc.
+```
+$ npm run lint
+$ npm run format
+```
+
+
---
+++ packages/sales-api/.eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir : __dirname, 
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    "@typescript-eslint/quotes": [
+      "error",
+      "single",
+      { 
+        "allowTemplateLiterals": true,
+        "avoidEscape": true
+      }
+    ],
+    "@typescript-eslint/explicit-member-accessibility": [
+      "off",
+      {
+        "accessibility": "explicit"
+      }
+    ],
+    "import/order": "off",
+    "max-len": [
+      "error",
+      {
+        "code": 150
+      }
+    ],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
